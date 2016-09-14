b3e5a6f276d40ee8818ad9e051a28a809a0c00f4
Monika Gupta <monikag@yahoo-inc.com>
2016-09-14 10:26:16 -0700

    Initial Commit

---
+++ InstaTumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaTumblr
+//
+//  Created by Monika Gupta on 9/14/16.
+//  Copyright (c) 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaTumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaTumblr
+//
+//  Created by Monika Gupta on 9/14/16.
+//  Copyright (c) 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
